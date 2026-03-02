8cf94bacb2c5ef7c1c7b23f23b16fc6bb8d2341f
Ivars Ozols <ivars.ozols.db@gmail.com>
2026-03-02 21:29:21 +0200

    Broken CI

--- test_kalkulators.py
+++ test_kalkulators.py
@@ -4,16 +4,16 @@ from kalkulators import saskaitit
 
 class TestKalkulators(unittest.TestCase):
     def test_saskaitit_pozitivi(self):
-        self.assertEqual(saskaitit(2, 3), 5)
+        self.assertEqual(saskaitit(2, 3), 6)
 
     def test_saskaitit_negativi(self):
-        self.assertEqual(saskaitit(-2, -3), -5)
+        self.assertEqual(saskaitit(-2, -3), -6)
 
     def test_saskaitit_nulle(self):
-        self.assertEqual(saskaitit(0, 5), 5)
+        self.assertEqual(saskaitit(0, 5), 6)
 
     def test_saskaitit_jaukti_skaitli(self):
-        self.assertEqual(saskaitit(-2, 3), 1)
+        self.assertEqual(saskaitit(-2, 3), 11)
 
 
 if __name__ == "__main__":
